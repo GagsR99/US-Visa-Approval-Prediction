b2e7ff79af10d5c5699678c771047ad63bc3cb7a
Gagan Rasineni <rasinenigagan@gmail.com>
2024-07-06 23:52:32 +0530

    Data Ingestion Added

--- README.md
+++ README.md
@@ -28,3 +28,19 @@ pip install -r requirements
 ```bash
 python app.py
 ```
+
+## Workflow
+
+1. constant
+2. config_entity
+3. artifact_entity
+4. component
+5. pipeline
+6. app.py
+
+## Export the environment variables
+```bash
+
+export MONGODB_URL = "mongodb+srv://<username>:<password>@cluster0.5o80sus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+```
--- demo.py
+++ demo.py
@@ -1,13 +1,20 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+# # from us_visa.logger import logging
+# # from us_visa.exception import USvisaException
+# # import sys
+# #
+# # try:
+# #     a = 1 / "10"
+# # except Exception as e:
+# #     logging.info(e)
+# #     raise USvisaException(e, sys) from e
+# #
+# # # logging.info("Welcome to our custom log")
 
-try:
-    a = 1 / "10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
+# from us_visa.constants import *
 
+# print(COLLECTION_NAME)
 
+from us_visa.pipeline.training_pipeline import TrainPipeline
 
-# logging.info("Welcome to our custom log")
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
